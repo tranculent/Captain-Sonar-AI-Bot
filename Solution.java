7c964c6dcb15d4cae51487d6868f0fc8c3520653
Yanko Mirov <45804656+tranculent@users.noreply.github.com>
2020-04-08 19:23:45 +0300

    added Torpedo Radius Available Locations Scanner
    Scans all the available cells that are in the radius of 4.

--- Solution.java
+++ Solution.java
@@ -76,6 +76,95 @@ class Player {
     static List<Location> visitedLocations = new ArrayList<>();
     static int DIRECTIONS = 4;
     
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int width = in.nextInt();
+        int height = in.nextInt();
+        int myId = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        
+        Map<Location, Character> islandLocations = storeMap(height, width, in);
+        
+        System.out.println("7 7");
+        
+        int numberOfMoves = 0;
+        int spawn = 0;
+        
+        // game loop
+        while (true) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int myLife = in.nextInt();
+            int oppLife = in.nextInt();
+            int torpedoCooldown = in.nextInt();
+            int sonarCooldown = in.nextInt();
+            int silenceCooldown = in.nextInt();
+            int mineCooldown = in.nextInt();
+            String sonarResult = in.next();
+            if (in.hasNextLine()) {
+                in.nextLine();
+            }
+            String opponentOrders = in.nextLine();
+            
+            List<Character> validMoves = validDirections(x, y, height, width, islandLocations);
+            
+            if (spawn++ == 0) visitedLocations.add(new Location(x, y));
+            
+            if (!sonarResult.equals("NA")) {
+                
+            } 
+            else {
+                int random;
+                if (!validMoves.isEmpty()) {
+                    random = new Random().nextInt(validMoves.size());
+                    if (validMoves.get(random) == 'E') {
+                       visitedLocations.add(new Location(x + 1, y));
+                    }
+                    else if (validMoves.get(random) == 'W') {
+                        visitedLocations.add(new Location(x - 1, y));
+                    }
+                    else if (validMoves.get(random) == 'N') {
+                        visitedLocations.add(new Location(x, y - 1));
+                    }
+                    else if (validMoves.get(random) == 'S') {
+                        visitedLocations.add(new Location(x, y + 1));
+                    }
+                    
+                    List<Location> temp = getAvailableTorpedoLocations(x, y, height, width, islandLocations);
+                    
+                    for (Location l : temp) {
+                     System.err.println("x: " + l.x + " ; y:" + l.y);   
+                    }
+                    
+                    System.out.println("MOVE " + validMoves.get(random) + " TORPEDO");
+                }
+                else {
+                    System.out.println("SURFACE");
+                    visitedLocations.clear();
+                    visitedLocations.add(new Location(x, y));
+                }
+            }
+        }
+    }
+    
+    public static Map<Location, Character> storeMap(int height, int width, Scanner in) {
+        Map<Location, Character> map = new HashMap<>();
+        for (int i = 0; i < height; i++) {
+            String line = in.nextLine();
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == 'x') {
+                    map.put(new Location(j, i), 'x');
+                }
+                else {
+                    map.put(new Location(j, i), '.');
+                }
+            }
+        }
+        return map;
+    }
+    
     public static List<Character> validDirections(int startingX, int startingY, int height, int width, Map<Location, Character> islandLocations) {
         List<Character> directions = new ArrayList<>();
         Map<Character, Boolean> visitedDirection = new HashMap<>();
@@ -118,189 +207,91 @@ class Player {
         return directions;
     }
     
-    public static List<Location> getTorpedoLocations(int x, int y, int height, int width, Map<Location, Character> islandLocations) {
-        List<Location> locations = new ArrayList<Location>();
+    public static List<Location> getAvailableTorpedoLocations(int x, int y, int height, int width, Map<Location, Character> islandLocations) {
+        List<Location> availableTorpedoLocations = new ArrayList<Location>();
         int radius = 4;
         int east = x, west = x;
         int south = y, north = y;
+            
+        int northRow = y > 0 ? y - 1 : 0;
+        int eastRow = x < width - 1 ? x + 1 : width - 1;
+        int westRow = x > 0 ? x - 1 : 0;
+        int southRow = y < height - 1 ? y + 1 : height;
         
-        // 4 places to the left (west)/4 places to the right (east)/4 places to the top (north)/4 places to the bottom (south)
-        // while (radius-- > 0) {
-            // Location newEast = new Location((east++) + 1, y);
-            // Location newWest = new Location((west--) - 1, y);
-            // Location newNorth = new Location(x, (north--) - 1);
-            // Location newSouth = new Location(x, (south++) + 1);
+        // handle NorthEast
+        int copyX = x;
+        System.err.println("North West: ");
+        for (int i = y - 5; i < y; i++) {
+            int copyI = i;
             
-            
-            int northRow = y > 0 ? y - 1 : 0;
-            int eastRow = x < width - 1 ? x + 1 : width - 1;
-            int westRow = x > 0 ? x - 1 : 0;
-            int southRow = y < height - 1 ? y + 1 : height;
-            
-            // handle NorthEast
-            // Start with: Location (y-1, x+1)
-            for (int i = northRow; i > northRow - 4; i--) {
-                System.err.println("In north east");
-                for (int j = eastRow; j < eastRow + 4; j++) {
-                    Location newLocation = new Location(y,x);
-                    if (islandLocations.containsKey(newLocation)) {
-                        if (islandLocations.get(newLocation) != 'x') {
-                            locations.add(newLocation);
-                        }
-                    }
-                }
+            while (copyI++ < y) {
+               Location newLocation = new Location(copyI, copyX);
+               if (islandLocations.containsKey(newLocation))
+               {
+                   if (islandLocations.get(newLocation) != 'x') {
+                        availableTorpedoLocations.add(newLocation);
+                   }
+               }
             }
-            
-            // handle NorthWest
-            // Start with: Location (y-1, x-1)
-            for (int i = northRow; i > northRow - 4; i--) {
-                System.err.println("In north west");
-                for (int j = westRow; j > westRow - 4; j--) {
-                    Location newLocation = new Location(y,x);
-                    if (islandLocations.containsKey(newLocation)) {
-                        if (islandLocations.get(newLocation) != 'x') {
-                            locations.add(newLocation);
-                        }
-                    }
-                }
-            }
-            
-            // handle SouthEast
-            for (int i = southRow; i < southRow + 4; i++) {
-                System.err.println("In south east");
-                for (int j = eastRow; j > eastRow - 4; j--) {
-                    Location newLocation = new Location(y,x);
-                    if (islandLocations.containsKey(newLocation)) {
-                        if (islandLocations.get(newLocation) != 'x') {
-                            locations.add(newLocation);
-                        }
-                    }
-                }
-            }
-            
-            // handle SouthWest
-            for (int i = southRow; i < southRow + 4; i++) {
-                System.err.println("In south west");
-                for (int j = eastRow; j < eastRow + 4; j++) {
-                    Location newLocation = new Location(y,x);
-                    if (islandLocations.containsKey(newLocation)) {
-                        if (islandLocations.get(newLocation) != 'x') {
-                            locations.add(newLocation);
-                        }
-                    }
-                }
-            }
-            
-            // Check for an island 'x' mark.
-            
-            // if (islandLocations.containsKey(newWest)) {
-            //     if (islandLocations.get(newWest) != 'x') {
-            //         // Add west
-            //         locations.add(newWest);
-            //     }
-            // }
-            // if (islandLocations.containsKey(newNorth)) {
-            //     if (islandLocations.get(newNorth) != 'x') {
-            //         // Add north
-            //         locations.add(newNorth);
-            //     }
-            // }
-            // if (islandLocations.containsKey(newSouth)) {
-            //     if (islandLocations.get(newSouth) != 'x') {
-            //         // Add south
-            //         locations.add(newSouth);
-            //     }
-            // }
-        // }
-        
-        return locations;
-    }
-    
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int width = in.nextInt();
-        int height = in.nextInt();
-        int myId = in.nextInt();
-        if (in.hasNextLine()) {
-            in.nextLine();
+            copyX--;
         }
         
-        Map<Location, Character> islandLocations = new HashMap<>();
-        
-        for (int i = 0; i < height; i++) {
-            String line = in.nextLine();
-            for (int j = 0; j < line.length(); j++) {
-                if (line.charAt(j) == 'x') {
-                    islandLocations.put(new Location(j, i), 'x');
-                }
-                else {
-                    islandLocations.put(new Location(j, i), '.');
-                }
-            }
-        }
-
-        System.out.println("7 7");
-        
-        int numberOfMoves = 0;
-        int spawn = 0;
-        
-        // game loop
-        while (true) {
-            int x = in.nextInt();
-            int y = in.nextInt();
-            int myLife = in.nextInt();
-            int oppLife = in.nextInt();
-            int torpedoCooldown = in.nextInt();
-            int sonarCooldown = in.nextInt();
-            int silenceCooldown = in.nextInt();
-            int mineCooldown = in.nextInt();
-            String sonarResult = in.next();
-            if (in.hasNextLine()) {
-                in.nextLine();
-            }
-            String opponentOrders = in.nextLine();
+        // handle NorthWest
+        int copyX1 = x;
+        System.err.println("North East: ");
+        for (int i = y + 5; i > y; i--) {
+            int copyI = i;
             
-            List<Character> validMoves = validDirections(x, y, height, width, islandLocations);
-            
-            if (spawn++ == 0) visitedLocations.add(new Location(x, y));
-                // Run command based on the opponents order
-            if (!sonarResult.equals("NA")) {
-                
-            } 
-            else {
-                int random;
-                if (!validMoves.isEmpty()) {
-                    random = new Random().nextInt(validMoves.size());
-                    if (validMoves.get(random) == 'E') {
-                       visitedLocations.add(new Location(x + 1, y));
-                    }
-                    else if (validMoves.get(random) == 'W') {
-                        visitedLocations.add(new Location(x - 1, y));
-                    }
-                    else if (validMoves.get(random) == 'N') {
-                        visitedLocations.add(new Location(x, y - 1));
-                    }
-                    else if (validMoves.get(random) == 'S') {
-                        visitedLocations.add(new Location(x, y + 1));
-                    }
-                    
-                    List<Location> temp = getTorpedoLocations(x, y, height, width, islandLocations);
-                    
-                    for (Location l : temp) {
-                     System.err.println("x: " + l.x + " ; y:" + l.y);   
-                    }
-                    
-                    System.out.println("MOVE " + validMoves.get(random) + " TORPEDO");
-                }
-                // If no moves are available
-                else {
-                    System.out.println("SURFACE");
-                    visitedLocations.clear();
-                    visitedLocations.add(new Location(x, y));
-                }
-                // If numberOfMoves < 3 (we can't fire a torpedo), move towards opponent
-                // If numberOfMoves == 3 (we have torpedo), look for the opponent sector (by using the sonarResult)0
+            while (copyI-- > y) {
+               Location newLocation = new Location(copyI, copyX1);
+               if (islandLocations.containsKey(newLocation))
+               {
+                   if (islandLocations.get(newLocation) != 'x') {
+                        availableTorpedoLocations.add(newLocation);
+                   }
+               }
             }
+            copyX1--;
         }
+        
+        // handle SouthWest
+        int copyX3 = x - 5;
+        int counter = 0;
+        System.err.println("South West: ");
+        for (int i = y; i < y + 5; i++) {
+            int copyI = i;
+            while (copyX3++ < x) {
+                Location newLocation = new Location(copyX3, copyI);
+                if (islandLocations.containsKey(newLocation))
+               {
+                   if (islandLocations.get(newLocation) != 'x') {
+                        availableTorpedoLocations.add(newLocation);
+                   }
+               }
+            }
+            counter++;
+            copyX3 = (x - 5) + counter;
+        }
+        
+        // handle SouthEast
+        int copyX2 = x;
+        System.err.println("South East: ");
+        for (int i = y + 5; i > y; i--) {
+            int copyI = i;
+            
+            while (copyI-- > y) {
+                Location newLocation = new Location(copyI, copyX2);
+                if (islandLocations.containsKey(newLocation))
+                {
+                   if (islandLocations.get(newLocation) != 'x') {
+                        availableTorpedoLocations.add(newLocation);
+                   }
+                }
+            }
+            
+            copyX2++;
+        }
+        
+        return availableTorpedoLocations;
     }
 }
